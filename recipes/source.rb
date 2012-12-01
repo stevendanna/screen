19f76a8c7fd8ff75cb522030e4f5452c8c3d823f
Allan Espinosa <allan.espinosa@outlook.com>
2012-12-01 12:10:43 +0900

    [COOK-1996] Add source recipe
    Use the screen git repository to build and install screen.

---
+++ recipes/source.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: screen
+# Recipe:: source
+#
+# Copyright (C) 2012 Allan Espinosa
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "git"
+include_recipe "build-essential"
+
+basedir = File.join Chef::Config.file_cache_path, "screen"
+
+git "screen" do
+  destination basedir
+  repository "http://git.savannah.gnu.org/r/screen.git"
+  revision "HEAD"
+end
+
+source_directory = File.join basedir, "src"
+
+execute "autoheader screen source" do
+  cwd source_directory
+  command "autoheader"
+  not_if { File.exist? File.join(source_directory, "config.h.in") }
+end
+
+execute "autoconf screen source" do
+  cwd source_directory
+  command "autoconf"
+  not_if { File.exist? File.join(source_directory, "configure") }
+end 
+
+execute "configure screen" do
+  install_prefix = File.join "usr", "local"
+  cwd source_directory
+  command "./configure --prefix=#{install_prefix}"
+  not_if { File.exist? File.join(source_directory, "Makefile") }
+end
+
+execute "compile screen" do
+  cwd source_directory
+  command "make"
+  not_if { File.exist? File.join(source_directory, "screen") }
+end
+
+execute "install screen" do
+  cwd source_directory
+  command "make install"
+  not_if { File.exist? File.join("usr", "local", "screen") }
+end
